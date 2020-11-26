5bcb4fe51d7e907f305b26be8ee4cdeb9d6d5af1
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-11-26 16:17:54 +0100

    assignment 2.4.2

---
+++ week_2/assignment_2_4_2.py
@@ -0,0 +1,20 @@
+distance = float(input("What is the distance you need to travel (in km)? "))
+
+walking_speed = 5       # in km/h
+walking_get_going = 1   # in minutes
+walking_parking = 0     # in minutes
+walking_total_time = walking_get_going + 60 * (distance / walking_speed) + walking_parking
+
+biking_speed = 20
+biking_get_going = 5
+biking_parking = 5
+biking_total_time = biking_get_going + 60 * (distance / biking_speed) + biking_parking
+
+driving_speed = 40      # in km/h, average within city limits?
+driving_get_going = 5
+driving_parking = 10
+driving_total_time = driving_get_going + 60 * (distance / driving_speed) + driving_parking
+
+print("Walking will take", walking_total_time, "minutes")
+print("Biking will take", biking_total_time, "minutes")
+print("Driving will take", driving_total_time, "minutes")
